7f07a0fb33e5a1b6ada541b25ade1dbee8a42835
axk167131 <axk167131@LIB253817.campus.ad.utdallas.edu>
2017-09-24 16:25:45 -0500

    changed the test module.

--- src/main/java/com/cometsale/test/test.java
+++ src/main/java/com/cometsale/test/test.java
@@ -15,24 +15,68 @@
  */
 
 package com.cometsale.test;
+import com.cometsale.model.Address;
 import com.cometsale.model.UserDetails;
 import com.cometsale.mongodb.UserDB;
+import com.cometsale.uimodel.UserResponseModel;
 
 
 
 public class test {
   
     public static void main(final String[] args) {
-      UserDetails userA= new UserDetails("test121241","testuser","t","est");
-      UserDB.push(userA);
-      
-        UserDetails findResult= UserDB.find("test121241","netID").get(0);
-        System.out.println("firstName:"+findResult.getFirstName()+"\nNetID:"+findResult.getNetId());
-        
-        UserDB.update("test121241","netID","updateFirstName", "firstName");
-      
-        findResult=(UserDetails) UserDB.find("test121241","netID").get(0);
-        System.out.println("firstName:"+findResult.getFirstName()+"\nNetID:"+findResult.getNetId());
+  
+//      
+//		UserDetails newUser = new UserDetails();
+//		
+//		newUser.setNetId("20237865");
+//		//newUser.setUserName(request.getParameter("username"));
+//		newUser.setFirstName("Akhilesh");
+//		newUser.setLastName("Kumar");
+//		//TODO:password.
+//		newUser.setEmail("akhi.x319@gmail.com");
+//		newUser.setPhoneNumber("4696644745");
+//		
+//		Address houseAddress = new Address();
+//		houseAddress.setCity("Dallas");
+//		houseAddress.setPinCode("75252");
+//		houseAddress.setState("Richardson");
+//		houseAddress.setStreetname("7815 McCallum");
+//		houseAddress.setAptNo("16101");
+//		newUser.setHomeAddress(houseAddress);
+//		
+//		UserDB.push(newUser);
+//		
+//		
+		UserDetails newUser2 = new UserDetails();
+		
+		newUser2.setNetId("202378651");
+		//newUser.setUserName(request.getParameter("username"));
+		newUser2.setFirstName("Akhilesh1");
+		newUser2.setLastName("Kumar");
+		//TODO:password.
+		newUser2.setEmail("akhi.x319@gmail.com");
+		newUser2.setPhoneNumber("4696644745");
+		
+		Address houseAddress2 = new Address();
+		houseAddress2.setCity("Dallas");
+		houseAddress2.setPinCode("75252");
+		houseAddress2.setState("Richardson");
+		houseAddress2.setStreetname("7815 McCallum");
+		houseAddress2.setAptNo("16101");
+		newUser2.setHomeAddress(houseAddress2);
+		System.out.println(newUser2);
+		
+		UserDB.push(newUser2);
+		
+   
+	  UserDetails findResult= UserDB.find("test121241","netID").get(0);
+	  System.out.println("firstName:"+findResult.getFirstName()+"\nNetID:"+findResult.getNetId());
+	    
+	  UserDB.update("test121241","netID","updateFirstName", "firstName");
+	  
+	  findResult=(UserDetails) UserDB.find("test121241","netID").get(0);
+	  System.out.println("firstName:"+findResult.getFirstName()+"\nNetID:"+findResult.getNetId());
         
     }
 }
