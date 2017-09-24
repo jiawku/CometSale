b19e5c971b9fd618a402df7ef5136e8db86faff9
axk167131 <axk167131@LIB253817.campus.ad.utdallas.edu>
2017-09-24 00:56:31 -0500

    commit files

--- src/main/java/com/cometsale/controller/UserController.java
+++ src/main/java/com/cometsale/controller/UserController.java
@@ -11,19 +11,19 @@ public class UserController {
 	@RequestMapping(value = "/addUser" ,method = RequestMethod.GET)
 	public String addNewUser(ModelMap model) {
 		System.out.println("entered add user page");
-		return "welcome-page";
+		return "contactus";
 	}
 	
 	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
 		System.out.println("entered normal");
-		return "welcome-page";
+		return "contactus";
 	}
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String welcome2(ModelMap model) {
 		System.out.println("entered normal");
-		return "welcome-page";
+		return "contactus";
 	}
 
 }
---
+++ src/main/java/com/cometsale/test/HelloControllerTests.java
@@ -0,0 +1,10 @@
+package com.cometsale.test;
+
+public class HelloControllerTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
